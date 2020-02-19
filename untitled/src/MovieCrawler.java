524aecf4d8cbc26cd64dbc507ac5da8d9da82d21
1317264 <30828669+1317264@users.noreply.github.com>
2020-02-19 22:17:24 +0800

    Update MovieCrawler.java

--- untitled/src/MovieCrawler.java
+++ untitled/src/MovieCrawler.java
@@ -11,20 +11,19 @@ import java.net.URLConnection;
 public class MovieCrawler {
     public static void main(String[] args) {
         Mysql mysql =new Mysql();
-        String sql;
         int i,j;
         int start=1292047;
         int end=1292067;//爬取电影id范围
         for(i=start;i<=end;i++){
-            String url="https://movie.douban.com/subject/"+String.valueOf(i)+"/";
+            String strurl="https://movie.douban.com/subject/"+String.valueOf(i)+"/";
             try{
-                HttpsUrlValidator.retrieveResponseFromServer(url);//忽略证书
-                Document doc= Jsoup.connect(url)
+                HttpsUrlValidator.retrieveResponseFromServer(strurl);//忽略证书
+                Document doc= Jsoup.connect(strurl)
                         .userAgent("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; MALC)")
                         .timeout(999999999)
                         .get();
                 //下载电影封面
-                System.out.println(url);
+                System.out.println(strurl);
                 Elements elements=doc.select("[class=nbgnbg]>img");
                 String imgurl=elements.attr("src");
                 String fileName = imgurl.substring(imgurl.lastIndexOf('/') + 1, imgurl.length());
@@ -47,20 +46,20 @@ public class MovieCrawler {
                 }
                 //获取部分数据存到数据库
                 elements=doc.select("[property=v:itemreviewed]");
-                String title=elements.first().text().replace("'","\\'");
+                String title=elements.first().text().replace("'","\'");
                 elements=doc.select("[property=v:runtime]");
                 String time;
                 if(elements.isEmpty())
                     time=null;
                 else
                     time=elements.first().text();
-                elements=doc.getElementsByClass("rating_sum");
+                elements=doc.select("[class=rating_sum]");
                 if(!elements.first().text().equals("暂无评分")){
                     elements=doc.select("[property=v:average]");
                     Double score=Double.parseDouble(elements.first().text());
                     elements=doc.select("[property=v:votes]");
                     int scorenum=Integer.parseInt(elements.first().text());
-                    elements=doc.getElementsByClass("rating_per");
+                    elements=doc.select("[class=rating_per]");
                     String star[] = new String[5];
                     j=0;
                     for(Element element:elements){
@@ -80,26 +79,26 @@ public class MovieCrawler {
                     if(elements1.text().equals("导演")){
                         Elements elements2=doc.select("[rel=v:directedBy]");
                         for(j=0;j<elements2.size();j++) {
-                            String director = elements2.get(j).text().replace("'","\\'");
-                            sql = "insert into director values(" + String.valueOf(i) + "," + String.valueOf(j + 1) + ",'" + director + "')";
-                            mysql.Save(sql);
+                            String director = elements2.get(j).text().replace("'","\'");
+                            mysql.SaveDirector(i,j+1,director);
                         }
+                        continue;
                     }
                     else if(elements1.text().equals("编剧")){
                         Elements elements2=element.select("[class=attrs]>a");
                         for(j=0;j<elements2.size();j++) {
-                            String screenwriter = elements2.get(j).text().replace("'","\\'");
-                            sql = "insert into screenwriter values(" + String.valueOf(i) + "," + String.valueOf(j + 1) + ",'" + screenwriter + "')";
-                            mysql.Save(sql);
+                            String screenwriter = elements2.get(j).text().replace("'","\'");
+                            mysql.SaveScreenwriter(i,j+1,screenwriter);
                         }
+                        continue;
                     }
                     else if(elements1.text().equals("主演")){
                         Elements elements2=doc.select("[rel=v:starring]");
                         for(j=0;j<elements2.size();j++) {
-                            String actor = elements2.get(j).text().replace("'","\\'");
-                            sql = "insert into actor values(" + String.valueOf(i) + "," + String.valueOf(j + 1) + ",'" + actor + "')";
-                            mysql.Save(sql);
+                            String actor = elements2.get(j).text().replace("'","\'");
+                            mysql.SaveActor(i,j+1,actor);
                         }
+                        continue;
                     }
                 }
                 System.out.println("完成");
@@ -110,5 +109,3 @@ public class MovieCrawler {
         }
     }
 }
-
-
